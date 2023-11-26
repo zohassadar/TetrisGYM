4679f6ff4820d0e841b725ca771ac7224a53f075
Richard <zohassadar@gmail.com>
2023-11-26 12:28:42 -0700

    rename rom when building

--- ed2ntc.sh
+++ ed2ntc.sh
@@ -4,6 +4,8 @@ set -e
 
 bash build.sh -e
 
+cp tetris.nes ed2ntc.nes
+
 # create IPS patch
-tools/flips-linux --create -i clean.nes tetris.nes ed2ntc.ips
+tools/flips-linux --create -i clean.nes ed2ntc.nes ed2ntc.ips
 
