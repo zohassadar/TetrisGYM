12b1a38d941a8d8a569cbe0585fc4cfe9b4ef058
Richard <zohassadar@gmail.com>
2024-02-21 09:29:08 -0700

    clean up; better percentages

--- src/util/random10.py
+++ src/util/random10.py
@@ -1,20 +1,19 @@
 from pprint import pprint
 
+
 def generate_next(hi, lo):
     newbit = ((hi & 2) ^ (lo & 2)) << 6
     return newbit | (hi >> 1), ((hi & 1) << 7) | (lo >> 1)
 
 
-seed_hi = 0x00
-seed_lo = 0x02
+seed_hi, seed_lo = 0, 2
 
 max_rolls = 0
 results = {}
 hi, lo = seed_hi, seed_lo
 
 while True:
-    tmp_hi = hi
-    tmp_lo = lo
+    tmp_hi, tmp_lo = hi, lo
     rolls = 0
     while True:
         rolls += 1
@@ -30,26 +29,22 @@ while True:
         break
 
 total = sum(results.values())
-results = {key: f'{total/value:02f}' for key, value in results.items()}
+results = {key: f"{(value/total)*100:.05f}" for key, value in results.items()}
 print("Existing: ")
 pprint(results)
 print(f"Max rolls: {max_rolls}")
 print("")
 
-max_rolls = 0
 results = {}
 hi, lo = seed_hi, seed_lo
 
 while True:
-    tmp_hi = hi
+    tmp_hi, tmp_lo = hi, lo
     tmp_lo = lo
-    i = 0
     for _ in range(5):
-        i += 1
         for _ in range(5):
             tmp_hi, tmp_lo = generate_next(tmp_hi, tmp_lo)
         result = tmp_lo & 15
-        max_rolls = max(max_rolls, i)
         if result < 10:
             break
     else:
@@ -60,7 +55,6 @@ while True:
         break
 
 total = sum(results.values())
-results = {key: f'{total/value:02f}' for key, value in results.items()}
+results = {key: f"{(value/total)*100:.05f}" for key, value in results.items()}
 print("Proposed:")
 pprint(results)
-print(f"Max rolls: {max_rolls}")
