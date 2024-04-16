5a023a29112fe9f55ca72c7941eef7bfd7e87de4
Richard <zohassadar@gmail.com>
2024-04-16 08:57:07 -0700

    Remove duplicate action

--- src/gamemodestate/branch.asm
+++ src/gamemodestate/branch.asm
@@ -14,10 +14,6 @@ branchOnGameModeState:
         .addr   gameModeState_vblankThenRunState2 ; gms: 2 acc eq (set to $2)
 
 gameModeState_next: ; used to be updatePlayer2
-.if SRVEDATG
-        jsr checkReceiveMove
-        jsr displayBetterMoveIfThere
-.endif
         inc gameModeState
         lda #$1 ; acc should not be equal
         rts
