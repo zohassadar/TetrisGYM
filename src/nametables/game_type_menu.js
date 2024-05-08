48627621aa2bde24190ce38a47f574156a60e46c
zohassadar <zohassadar@gmail.com>
2024-05-08 04:33:02 +0000

    Adjust url

--- src/nametables/game_type_menu.js
+++ src/nametables/game_type_menu.js
@@ -77,8 +77,8 @@ drawTiles(buffer, lookup, `
 #a    DARK MODE               d#
 #a    PAL MODE                d#
 #a                            d#
-#a                            d#
 #a V6                         d#
+#a                            d#
 #a                            d#
 #a                            d#
 #a                            d#
@@ -134,7 +134,7 @@ drawTiles(extra, lookup, background);
 drawRect(buffer, 8, 2, 10, 5, 0xB0); // draw logo
 
 const urlX = 3;
-const urlY = 15;
+const urlY = 16;
 drawRect(extra, urlX, urlY, 12, 1, 0x74);
 drawRect(extra, urlX+12, urlY, 12, 1, 0x84);
 
@@ -168,8 +168,17 @@ drawAttrs(extra, [`
     2222222222222222
     2222222222222222
     2222222222222222
+    2222222222222222
+`,`
     2333333333333332
-`, screen]);
+    2222222222222222
+    2222222222222222
+    2222222222222222
+    2222222222222222
+    2222222222222222
+    2222222222222222
+    2222222222222222
+`]);
 
 writeRLE(
     __dirname + '/game_type_menu_nametable_practise.bin',
