70d23e8b0dc413249f1ef9373179aeb0a52fb3c0
zohassadar <zohassadar@gmail.com>
2024-11-09 17:19:06 +0000

    count cycles for staging and dropping

--- tests/src/harddrop.rs
+++ tests/src/harddrop.rs
@@ -152,26 +152,31 @@ const BLANK_BOARD: &str = r##"
 
 pub fn test() {
     let mut emu = util::emulator(None);
-    let mut max_cycles: u32;
-    let mut cycles: u32;
+    let mut max_stage: u32;
+    let mut max_drop: u32;
+    let mut stage_cycles: u32;
+    let mut drop_cycles: u32;
 
-    cycles = test_harddropped_piece(&mut emu, TETRIS_READY, TEST1_AFTER, 0x11);
-    max_cycles = cycles;
+    (max_stage, max_drop) = test_harddropped_piece(&mut emu, TETRIS_READY, TEST1_AFTER, 0x11);
 
-    cycles = test_harddropped_piece(&mut emu, TETRIS_READY, TEST2_AFTER, 0xF);
-    max_cycles = if cycles > max_cycles { cycles } else { max_cycles };
+    (stage_cycles, drop_cycles) = test_harddropped_piece(&mut emu, TETRIS_READY, TEST2_AFTER, 0xF);
+    max_stage = if stage_cycles > max_stage { stage_cycles } else { max_stage };
+    max_drop = if drop_cycles > max_drop { drop_cycles } else { max_drop };
 
-    cycles = test_harddropped_piece(&mut emu, TETRIS_READY_PC, BLANK_BOARD, 0x11);
-    max_cycles = if cycles > max_cycles { cycles } else { max_cycles };
+    (stage_cycles, drop_cycles) = test_harddropped_piece(&mut emu, TETRIS_READY_PC, BLANK_BOARD, 0x11);
+    max_stage = if stage_cycles > max_stage { stage_cycles } else { max_stage };
+    max_drop = if drop_cycles > max_drop { drop_cycles } else { max_drop };
 
-    cycles = test_harddropped_piece(&mut emu, TETRIS_READY_FULL, TETRIS_FULL_AFTER, 0x11);
-    max_cycles = if cycles > max_cycles { cycles } else { max_cycles };
+    (stage_cycles, drop_cycles) = test_harddropped_piece(&mut emu, TETRIS_READY_FULL, TETRIS_FULL_AFTER, 0x11);
+    max_stage = if stage_cycles > max_stage { stage_cycles } else { max_stage };
+    max_drop = if drop_cycles > max_drop { drop_cycles } else { max_drop };
 
-    println!("Hard Drop max cycles: {}", max_cycles);
+    println!("Hard Drop max stage cycles: {}", max_stage);
+    println!("Hard Drop max drop cycles: {}", max_drop);
 
     }
 
-fn test_harddropped_piece(emu: &mut NesState, start: &str, finish: &str, piece: u8 ) -> u32 {
+fn test_harddropped_piece(emu: &mut NesState, start: &str, finish: &str, piece: u8 ) -> (u32,u32) {
     emu.reset();
 
     for _ in 0..3 { emu.run_until_vblank(); }
@@ -184,6 +189,7 @@ fn test_harddropped_piece(emu: &mut NesState, start: &str, finish: &str, piece: 
     let button_up = labels::get("BUTTON_UP") as u8;
     let newly_pressed_buttons = labels::get("newlyPressedButtons") as usize;
     let active_tetrimino = labels::get("playState_playerControlsActiveTetrimino");
+    let stage_sprite = labels::get("stageSpriteForCurrentPiece");
 
     emu.memory.iram_raw[practise_type] = mode_harddrop;
     emu.memory.iram_raw[game_mode] = 4;
@@ -196,17 +202,37 @@ fn test_harddropped_piece(emu: &mut NesState, start: &str, finish: &str, piece: 
     playfield::set_str(emu, start);
 
     emu.memory.iram_raw[labels::get("playState") as usize] = 1;
+    emu.memory.iram_raw[labels::get("currentPiece") as usize] = piece;
+    emu.memory.iram_raw[labels::get("tetriminoX") as usize] = 0x5;
+    emu.memory.iram_raw[labels::get("tetriminoY") as usize] = 0x0;
+    // stage ghost piece
+    let temp_pc = emu.registers.pc;
+    emu.registers.pc = stage_sprite;
+    let stage_cycles = util::cycles_to_return(emu);
+    emu.registers.pc = temp_pc;
+
+    util::run_n_vblanks(emu, 1);
+
+    emu.memory.iram_raw[labels::get("playState") as usize] = 1;
     emu.memory.iram_raw[labels::get("autorepeatY") as usize] = 0;
     emu.memory.iram_raw[labels::get("currentPiece") as usize] = piece;
     emu.memory.iram_raw[labels::get("tetriminoX") as usize] = 0x5;
     emu.memory.iram_raw[labels::get("tetriminoY") as usize] = 0x0;
     emu.memory.iram_raw[labels::get("vramRow") as usize] = 0x20;
     emu.memory.iram_raw[newly_pressed_buttons] = button_up;
-    emu.registers.pc = active_tetrimino;
 
-    let cycles = util::cycles_to_return(emu);
+    // hard drop and count cycles
+    emu.registers.pc = active_tetrimino;
+    let drop_cycles = util::cycles_to_return(emu);
+
+
+
+    println!("ghost y: {:X}", emu.memory.iram_raw[labels::get("hardDropGhostY") as usize]);
+    println!("tetrimino y: {:X}", emu.memory.iram_raw[labels::get("tetriminoY") as usize]);
+    println!("buttons: {:X}", emu.memory.iram_raw[labels::get("newlyPressedButtons") as usize]);
+
 
     assert_eq!(finish, playfield::get_str(emu));
 
-    return cycles;
+    return (stage_cycles, drop_cycles);
 }
