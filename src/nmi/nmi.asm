b777981445e8bfef382febdff1935ad800ee5ca8
zohassadar <zohassadar@gmail.com>
2025-01-27 20:39:38 +0000

    trim nmi

--- src/nmi/nmi.asm
+++ src/nmi/nmi.asm
@@ -3,37 +3,32 @@ nmi:    pha
         pha
         tya
         pha
-        lda #$00
-        sta oamStagingLength
         jsr render
-        lda currentPpuCtrl
-        sta PPUCTRL
-        dec sleepCounter
-        lda sleepCounter
-        cmp #$FF
-        bne @jumpOverIncrement
-        inc sleepCounter
-@jumpOverIncrement:
-        jsr copyOamStagingToOam
-        lda frameCounter
-        clc
-        adc #$01
-        sta frameCounter
+        jsr copyCurrentScrollAndCtrlToPPU
         lda #$00
-        adc frameCounter+1
-        sta frameCounter+1
+        sta OAMADDR
+        lda #$02
+        sta OAMDMA
+        lda sleepCounter
+        beq :+
+        dec sleepCounter
+:
+        inc frameCounter
+        bne :+
+        inc frameCounter+1
+:
         ldx #rng_seed
         jsr generateNextPseudorandomNumber
-        jsr copyCurrentScrollAndCtrlToPPU
         jsr pollControllerButtons
-        lda #$00
-        sta lagState ; clear flag after lag frame achieved
 .if KEYBOARD
 ; Read Family BASIC Keyboard
         jsr pollKeyboard
 .endif
-        lda #$01
-        sta verticalBlankingInterval
+        ldy #$00
+        sty oamStagingLength
+        sty lagState ; clear flag after lag frame achieved
+        iny          ; quick 1
+        sty verticalBlankingInterval
         pla
         tay
         tsx
