5a2c3ecd1b379afc806ce3e2369f33432d3d46bc
zohassadar <zohassadar@gmail.com>
2026-07-04 16:06:19 +0000

    document calibrate

--- src/modes/calibrate.asm
+++ src/modes/calibrate.asm
@@ -1,4 +1,93 @@
-advanceGameCalibrate:
+gameMode_calibrate:
+; stays in loop until ABSS
+
+@fillModifier = anydasFlag
+; 0-3, increases on up/down
+; rotates through a blank board or filled with each of the three minos
+; refreshes pattern if not in fill mode
+
+; levelNumber
+; 0-9, increase/decrease with left/right
+
+@fillFlag = tetriminoY
+; 0-1, toggled by B
+; 0 = show random pattern
+; 1 = fill board with @fillModifier
+
+@suspendRefresh = fallTimer
+; 0-1, toggled by start
+; 0 = refresh pattern every 128 frames
+; 1 = stay on current pattern
+
+@nextBoxPiece = tetriminoX
+; 0-6, increases on select and wraps around at 6
+; corresponding stats counter increases on each increment
+; hold A to rotate once per frame
+
+    jsr gameModeState_initGameBackground
+    jsr gameModeState_initGameState
+    ldx nextPiece
+    stx currentPiece
+    lda tetriminoTypeFromOrientation,x
+    sta @nextBoxPiece
+
+@refreshPattern:
+    lda #0
+    sta vramRow
+    lda @fillFlag
+    bne @tilefill
+    ldy #15
+@fill:
+    ldx #b_seed
+    jsr generateNextPseudorandomNumber5x
+    lda oneThirdPRNG
+    clc
+    adc #$7B
+    sta mathRAM,y
+    dey
+    bpl @fill
+
+    lda rng_seed+1
+    and #7
+    tax
+    lda #$EF
+    sta mathRAM,x
+    sta mathRAM+8,x
+
+    ldy #200
+    lda rng_seed
+    and #15
+    tax
+@loop:
+    lda mathRAM,x
+    sta playfield-1,y
+    dex
+    bpl :+
+    ldx #15
+:
+    dey
+    bne @loop
+    jmp @waitLoop
+
+@tilefill:
+    ldx #200
+    lda @fillModifier
+    beq @empty
+    lda #$7A
+    clc
+    adc @fillModifier
+    bne @tile
+@empty:
+    lda #EMPTY_TILE
+@tile:
+    sta playfield-1,x
+    dex
+    bne @tile
+    jmp @waitLoop
+
+@checkInputs:
+
+; reset sequence
     lda heldButtons_player1
     cmp #BUTTON_A+BUTTON_B+BUTTON_SELECT+BUTTON_START
     bne @noReset
@@ -6,15 +95,18 @@ advanceGameCalibrate:
     sta gameMode
     rts
 @noReset:
+
+; B to toggle @fillFlag
     lda newlyPressedButtons_player1
-    and #BUTTON_A|BUTTON_B
+    and #BUTTON_B
     beq @noToggle
-    lda tetriminoY
+    lda @fillFlag
     eor #1
-    sta tetriminoY
-    jmp refreshPattern
+    sta @fillFlag
+    jmp @refreshPattern
 @noToggle:
-    @fillModifier = anydasFlag  ; placeholder
+
+; up/down to rotate @fillModifier
     lda newlyPressedButtons_player1
     and #BUTTON_UP|BUTTON_DOWN
     beq @upNotPressed
@@ -22,39 +114,50 @@ advanceGameCalibrate:
     lda @fillModifier
     and #3
     sta @fillModifier
-    jmp refreshPattern
+    jmp @refreshPattern
 @upNotPressed:
+
+; start to suspend pattern refresh
     lda newlyPressedButtons_player1
     and #BUTTON_START
     beq @startNotPressed
-    lda fallTimer
+    lda @suspendRefresh
     eor #1
-    sta fallTimer
-    bne calibrateWaitLoop
-    jmp refreshPattern
+    sta @suspendRefresh
+    bne @waitLoop
+    jmp @refreshPattern
 @startNotPressed:
-    lda #BUTTON_SELECT
-    jsr menuThrottle
-    beq @selectNotPressed
+
+; select to rotate piece
+; A+select to rapdily rotate
+    lda newlyPressedButtons_player1
+    and #BUTTON_SELECT
+    bne @rotatePiece
+    lda heldButtons_player1
+    cmp #BUTTON_A+BUTTON_SELECT
+    bne @noPieceRotate
+
+@rotatePiece:
     lda nextPiece
     sta currentPiece
     jsr incrementPieceStat
-    inc tetriminoX
-    lda tetriminoX
+    inc @nextBoxPiece
+    lda @nextBoxPiece
     cmp #7
     bne @noRollover
     lda #0
-    sta tetriminoX
+    sta @nextBoxPiece
 @noRollover:
     tax
     lda spawnTable,x
     sta nextPiece
-    jmp calibrateWaitLoop
+    jmp @waitLoop
 
-@selectNotPressed:
+; left/right to decrease/increase levelNumber
+@noPieceRotate:
     lda newlyPressedButtons_player1
     and #3
-    beq calibrateCheckFrameCounter
+    beq @checkFrameCounter
     lsr
     bcs @rightPressed
 
@@ -100,81 +203,20 @@ advanceGameCalibrate:
     lda #RENDER_LINES|RENDER_LEVEL|RENDER_SCORE
     sta renderFlags
 
-calibrateWaitLoop:
+@waitLoop:
     jsr stageSpriteForNextPiece
     jsr updateAudioWaitForNmiAndResetOamStaging
-    jmp advanceGameCalibrate
+    jmp @checkInputs
 
 ; shuffle every 128 frames
-calibrateCheckFrameCounter:
-    lda fallTimer
-    bne calibrateWaitLoop
+@checkFrameCounter:
+    lda @suspendRefresh
+    bne @waitLoop
     lda frameCounter
     and #$7F
-    beq refreshPattern
-    jmp calibrateWaitLoop
+    beq @refresh
+    jmp @waitLoop
+@refresh:
+    jmp @refreshPattern
 
-gameMode_calibrate:
-    jsr gameModeState_initGameBackground
-    jsr gameModeState_initGameState
-    ldx nextPiece
-    stx currentPiece
-    lda tetriminoTypeFromOrientation,x
-    sta tetriminoX
-
-refreshPattern:
-    @fillModifier = anydasFlag
-    lda #0
-    sta vramRow
-    lda tetriminoY
-    bne @tilefill
-    ldy #15
-@fill:
-    ldx #b_seed
-    jsr generateNextPseudorandomNumber5x
-    lda oneThirdPRNG
-    clc
-    adc #$7B
-    sta mathRAM,y
-    dey
-    bpl @fill
-
-    lda rng_seed+1
-    and #7
-    tax
-    lda #$EF
-    sta mathRAM,x
-    sta mathRAM+8,x
-
-    ldy #200
-    lda rng_seed
-    and #15
-    tax
-@loop:
-    lda mathRAM,x
-    sta playfield-1,y
-    dex
-    bpl :+
-    ldx #15
-:
-    dey
-    bne @loop
-    jmp calibrateWaitLoop
-
-@tilefill:
-    ldx #200
-    lda @fillModifier
-    beq @empty
-    lda #$7A
-    clc
-    adc @fillModifier
-    bne @tile
-@empty:
-    lda #EMPTY_TILE
-@tile:
-    sta playfield-1,x
-    dex
-    bne @tile
-    jmp calibrateWaitLoop
-
-.out .sprintf("Calibrate code: %d", *-advanceGameCalibrate)
+.out .sprintf("Calibrate code: %d", *-gameMode_calibrate)
