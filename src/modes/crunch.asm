c35729b9ae730d9145bc18cf1b4cb4a9fefc557d
zohassadar <zohassadar@gmail.com>
2026-07-31 17:54:58 +0000

    comment no longer relevant

--- src/modes/crunch.asm
+++ src/modes/crunch.asm
@@ -1,21 +1,3 @@
-; crunch start variants:
-; 0 - 0 left 0 right
-; 1 - 0 left 1 right
-; 2 - 0 left 2 right
-; 3 - 0 left 3 right
-; 4 - 1 left 0 right
-; 5 - 1 left 1 right
-; 6 - 1 left 2 right
-; 7 - 1 left 3 right
-; 8 - 2 left 0 right
-; 9 - 2 left 1 right
-; A - 2 left 2 right
-; B - 2 left 3 right
-; C - 3 left 0 right
-; D - 3 left 1 right
-; E - 3 left 2 right
-; F - 3 left 3 right
-
 ; clobbers generalCounter3 & generalCounter4 (defined in playstate/util.asm)
 
 initGameCrunch:
