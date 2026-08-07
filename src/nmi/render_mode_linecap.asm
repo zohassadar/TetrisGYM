313d4f153c173c756e63f17ecabca9479e32620d
zohassadar <zohassadar@gmail.com>
2026-08-07 23:05:44 +0000

    remove unused

--- src/nmi/render_mode_linecap.asm
+++ src/nmi/render_mode_linecap.asm
@@ -1,19 +1,3 @@
-render_mode_linecap_menu:
-        lda renderFlags
-        and #RENDER_LINES
-        beq @static
-        ; render level / lines
-        lda #0
-        sta renderFlags
-        lda #$21
-        sta PPUADDR
-        lda #$F3
-        sta PPUADDR
-        jsr render_linecap_level_lines
-
-@static:
-        rts
-
 render_linecap_level_lines:
         lda linecapWhen
         cmp #LINECAP_LINES
